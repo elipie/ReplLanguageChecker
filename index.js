9a8bd7acd1729e79778483b920f32ff62c5afab0
Eli <72884616+elipie@users.noreply.github.com>
2020-12-10 15:01:12 -0600

    Finished!

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-const fetch = require('node-fetch')
+
+
+const fetch = require('node-fetch');
+
+extensions = [];
 
 async function getReplLangs(username, repl) {
-  let info = await fetch(`https://staging.repl.it/data/repls/@${username}/${repl}`, {
-    method: `GET`,
-    headers: {
-      'User-Agent': 'ReplLangChecker'
-    }
-  }).then(res => res.json()); 
+	let info = await fetch(
+		`https://staging.repl.it/data/repls/@${username}/${repl}`,
+		{
+			method: `GET`,
+			headers: {
+				'User-Agent': 'ReplLangChecker'
+			}
+		}
+	).then(res => res.json());
 
-  console.log(info)
+	if (info.fileNames == undefined) {
+		console.log('Private Repl, cannot view!');
+		return;
+	}
+
+	info.fileNames.forEach(filename => {
+		let extension = filename.split('.')[1];
+		extensions.push(extension);
+	});
+
+	let length = extensions.length;
+	let final = [];
+	extensions.forEach(extension => {
+		let same = extensions.filter(e => e == extension);
+		extensions = extensions.filter(e => e != extension);
+		final.push([extension, same.length, length]);
+	});
+
+	final.forEach(instance => {
+		if (instance[1] != 0) {
+			let percentage = Number(parseInt((instance[1] / instance[2]) * 10000)) / 100;
+			console.log(`${instance[0].toUpperCase()}: \n ${percentage}% / ${instance[1]} files \n---`);
+		}
+	});
 }
-// if you run this, you will see the `languages` key. We need to check that.
-getReplLangs('elipie', 'e-2')
+
+getReplLangs('RayhanADev', 'its');
